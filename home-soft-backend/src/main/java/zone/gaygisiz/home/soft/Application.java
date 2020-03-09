c7c76a153ba340673d2040d738646cb00c4dc2de
Resul Gaygisiz <resulgaygisiz@gmail.com>
2020-03-09 22:54:31 +0100

    #4 Dockerise backend with inmemory db

--- home-soft-backend/src/main/java/zone/gaygisiz/home/soft/Application.java
+++ home-soft-backend/src/main/java/zone/gaygisiz/home/soft/Application.java
@@ -2,10 +2,8 @@ package zone.gaygisiz.home.soft;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 public class Application {
 
 	public static void main(String[] args) {
